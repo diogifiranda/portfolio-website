fb4665471749d95a8ff8716934b6b2c4da0cb789
diogifiranda <diogifiranda04@gmail.com>
2023-01-09 20:09:07 +0700

    first commit

---
+++ resportf/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Saya Sri wahyuni dan Khairunnisa</span> Kami berdua merupakan Mahasiswa S1 Sistem Informasi di Universitas Islam indragiri hilir dan Kami berdua Sama-sama lulusan dari Smk Negeri 1 Tembilahan"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
